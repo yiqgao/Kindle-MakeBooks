86f5aada54e143a5dfdf87ed6395f3f6cc2f474d
Teige Gao <teige_gao@hotmail.com>
2018-03-13 14:55:43 +0800

    Add files via upload

---
+++ Bookchange.ps1
@@ -0,0 +1,11 @@
+﻿$web = $env:USERPROFILE+'\OneDrive\Sync\Work\Scripts\wordbook\part06.txt' 
+$temp = Get-Content $web 
+$b =0 
+$book = foreach ($item in $temp -match '<p class="calibre_4"><span class="bold">.*?</dddd>')
+{
+    $b++ 
+    $d = '<p id = "ch3-'+$b+'" class = "word_item">'+'<span class="word">'
+    $sub_item = $item -replace ('<p class="calibre_4"><span class="bold">',$d) -replace ('</p> ','</h2>') -replace ('</span><span class="bold"><span class="calibre_2">','<span class="yinbiao">')
+    $sub_item -replace ('</span></span><span class="bold">','</span>') -replace ('</dddd>','')
+}
+$book | Out-File 'C:\Users\v-yiqga\OneDrive\Sync\Work\Scripts\result_book\part06.txt'
